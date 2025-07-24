0ce71f620ae0dffdd71b303d4acb7dd125517cb6
BruceBoron <bruceboron666@gmail.com>
2025-07-24 08:09:40 +0800

    548. 将数组分割成和相等的子数组

---
+++ src/main/java/com/boron/hash/hard/SplitArray.java
@@ -0,0 +1,164 @@
+package com.boron.hash.hard;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <pre>
+ *  @description: 548. 将数组分割成和相等的子数组 <a href="https://leetcode.cn/problems/split-array-with-equal-sum/description/?envType=problem-list-v2&envId=hash-table">跳转<a/>
+ *  @author: BruceBoron
+ *  @date: 2025/7/24
+ * </pre>
+ */
+public class SplitArray {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    static class Param {
+        int[] nums;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    static class Result {
+        boolean result;
+    }
+
+    public static Pair<Param, Result> generate0() {
+        Param param = Param.builder().nums(new int[] {1,2,1,2,1,2,1}).build();
+        Result result = Result.builder().result(true).build();
+        return Pair.of(param, result);
+    }
+
+    public static Pair<Param, Result> generate1() {
+        Param param = Param.builder().nums(new int[] {1,2,1,2,1,2,1,2}).build();
+        Result result = Result.builder().result(false).build();
+        return Pair.of(param, result);
+    }
+
+    public static Pair<Param, Result> generate2() {
+        Param param = Param.builder().nums(new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}).build();
+        Result result = Result.builder().result(false).build();
+        return Pair.of(param, result);
+    }
+
+    public static Pair<Param, Result> generate3() {
+        Param param = Param.builder().nums(new int[] {0,0,0,0,0, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1}).build();
+        Result result = Result.builder().result(false).build();
+        return Pair.of(param, result);
+    }
+
+    public static Pair<Param, Result> generate4() {
+        Param param = Param.builder().nums(new int[] {0,9,3,4,-4, -3,-9,5,-4,8}).build();
+        Result result = Result.builder().result(false).build();
+        return Pair.of(param, result);
+    }
+
+    public static boolean test(Pair<Param, Result> testParam) {
+        Param param = testParam.getKey();
+        Result result = testParam.getValue();
+        boolean actualResult = SplitArraySolution.splitArray(param.getNums());
+        boolean expectResult = result.isResult();
+        boolean compareResult = actualResult == expectResult;
+        System.out.println("actualResults vs expectResults");
+        System.out.printf("%s vs %s\n", actualResult, expectResult);
+        System.out.println("compareResult: " + compareResult);
+        return compareResult;
+    }
+
+    public static void main(String[] args) {
+        test(generate0());
+        test(generate1());
+        test(generate2());
+        test(generate3());
+        test(generate4());
+    }
+}
+
+class SplitArraySolution {
+    public static boolean splitArray(int[] nums) {
+        // 最小长度为7
+        if (nums.length < 7) {
+            return false;
+        }
+        // 分别计算前缀和与后缀和
+        int prefixSum = 0;
+        int suffixSum = 0;
+        // 分别存入前缀和数组、后缀和数组
+        int[] prefixSumArr = new int[nums.length];
+        int[] suffixSumArr = new int[nums.length];
+        // 记录不同前缀和、后缀和对应的下标
+        Map<Integer, List<Integer>> prefixMap = new HashMap<>();
+        Map<Integer, List<Integer>> suffixMap = new HashMap<>();
+        // 遍历数组，将前缀和、后缀和都写入对应变量
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            prefixSumArr[i] = prefixSum;
+            List<Integer> prefixIndices = prefixMap.computeIfAbsent(prefixSum, k -> new ArrayList<>());
+            prefixIndices.add(i);
+
+            suffixSum += nums[nums.length - 1 - i];
+            suffixSumArr[i] = suffixSum;
+            List<Integer> suffixIndices = suffixMap.computeIfAbsent(suffixSum, k -> new ArrayList<>());
+            suffixIndices.add(nums.length - 1 - i);
+        }
+        // 第二次遍历数组
+        for (int i = 1; i < nums.length - 5; i++) {
+            // frame 1
+            // 获取第一个片段的和
+            int prefix1 = prefixSumArr[i - 1];
+            // frame 2
+            // 根据第一个片段和来计算第二个片段的前缀和
+            int _prefix2 = prefixSumArr[i] + prefix1;
+            // 如果前缀map中不包含该前缀和，则直接跳过
+            if (!prefixMap.containsKey(_prefix2)) {
+                continue;
+            }
+            // frame 4
+            // 因为第1个片段的和与第4个片段的和是相等的
+            // 如果后缀map中不包含该片段和，则直接跳过
+            if (!suffixMap.containsKey(prefix1)) {
+                continue;
+            }
+            // 分别获取对应的索引
+            List<Integer> indices2 = prefixMap.get(_prefix2);
+            List<Integer> indices4 = suffixMap.get(prefix1);
+            // 遍历第2个片段的索引
+            for (int _j : indices2) {
+                // 获取真实的j
+                int j = _j + 1;
+                // 条件i + 1 < j，如果不满足，直接跳过
+                if (i + 1 >= j) {
+                    continue;
+                }
+                // 遍历第4个片段的索引
+                for (int k_ : indices4) {
+                    // 获取真实的k
+                    int k = k_ - 1;
+                    // 条件 j + 1 < k 需满足
+                    if (j + 1 < k) {
+                        // 获取片段3的和
+                        int prefix3 = prefixSumArr[k - 1] - prefixSumArr[j];
+                        // 如果片段3的和与片段1的和一致，则返回true
+                        if (prefix3 == prefix1) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
