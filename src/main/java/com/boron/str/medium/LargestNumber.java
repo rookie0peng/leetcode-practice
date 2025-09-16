ec242489a8316671dda8ec587740dda44eaaf210
BruceBoron <bruceboron666@gmail.com>
2025-09-16 21:40:00 +0800

    179. 最大数

--- src/main/java/com/boron/str/medium/LargestNumber.java
+++ src/main/java/com/boron/str/medium/LargestNumber.java
@@ -61,6 +61,13 @@ public class LargestNumber {
         return Pair.of(param, result);
     }
 
+    // 8308,8308,830
+    public static Pair<Param, Result> generate4() {
+        Param param = Param.builder().nums(new int[] {8308,8308,830}).build();
+        Result result = Result.builder().result("83088308830").build();
+        return Pair.of(param, result);
+    }
+
     public static boolean test(Pair<Param, Result> testParam) {
         Param param = testParam.getKey();
         Result result = testParam.getValue();
@@ -75,11 +82,11 @@ public class LargestNumber {
 
     public static void main(String[] args) {
 
-//        test(generate0());
-//        test(generate1());
-//        test(generate2());
+        test(generate0());
+        test(generate1());
+        test(generate2());
         test(generate3());
-//        test(generate4());
+        test(generate4());
 //        test(generate5());
 //        test(generate6());
     }
@@ -89,32 +96,48 @@ public class LargestNumber {
 class LargestNumberSolution {
     public static String largestNumber(int[] nums) {
         int len = nums.length;
-        List<String> strNums = new ArrayList<>(len);
-        for (int num : nums) {
-            strNums.add(String.valueOf(num));
+        // 将整型数组转为字符串数组
+        String[] strNumArr = new String[len];
+        for (int i = 0; i < len; i++) {
+            strNumArr[i] = String.valueOf(nums[i]);
         }
+        // 排序
         List<String> sortedStrNums = new ArrayList<>(len);
-        sortedStrNums.add(strNums.get(0));
+        sortedStrNums.add(strNumArr[0]);
         for (int i = 1; i < len; i++) {
-            String num1 = strNums.get(i);
-            // 二分法排序
-            int left = 0, right = sortedStrNums.size();
-            int mid = (left + right) / 2;
-            while (left <= right) {
+            String num1 = strNumArr[i];
+            // 二分法排序，降序排序
+            // 初始化左端点和右端点
+            int left = 0, right = sortedStrNums.size() - 1;
+            // 二分点
+            int mid = 0;
+            // 比较结果
+            int compare = 0;
+            while (left >= 0 && left <= right) {
+                mid = (left + right) / 2;
                 String num2 = sortedStrNums.get(mid);
-                int maxLen = Math.max(num1.length(), num2.length());
-                int minLen = Math.min(num1.length(), num2.length());
-                int compare = dfsCompare(num1, num2, maxLen, minLen, 0);
+                compare = compare(num1, num2);
+                // num1大于num2，则右端点左移
                 if (compare == 1) {
                     right = mid - 1;
                 } else if (compare == -1) {
+                    // num1小于num2，则左端点左移
                     left = mid + 1;
                 } else {
                     break;
                 }
-                mid = (left + right) / 2;
             }
-            sortedStrNums.add(mid, num1);
+            // 如果num1小于num2，则将结果添加到中间点后一位。
+            if (compare == -1) {
+                sortedStrNums.add(mid + 1, num1);
+            } else {
+                // 如果num1大于等于num2，则将结果添加到中间点
+                sortedStrNums.add(mid, num1);
+            }
+        }
+        // 如果排序后，第一个数字为0，则直接返回0
+        if (Objects.equals(sortedStrNums.get(0), "0")) {
+            return "0";
         }
         StringBuilder sb = new StringBuilder();
         for (String num : sortedStrNums) {
@@ -123,53 +146,27 @@ class LargestNumberSolution {
         return sb.toString();
     }
 
-    public static int dfsCompare(String num1, String num2, int maxLen, int minLen, int step) {
-        int i = 0;
-        boolean _1less_2 = num1.length() < num2.length();
-        int right = Math.min(minLen, maxLen - step);
-        char c1, c2;
-        int idx1, idx2;
-        while (i < right) {
-            if (_1less_2) {
-                idx1 = i;
-                idx2 = i + step;
+    public static int compare(String num1, String num2) {
+        // 将两个字符串合并来看
+        // 分别比较 num1-num2 和 num2-num1 的大小
+        int len1 = num1.length(), len2 = num2.length();
+        long x1 = 0, x2 = 0;
+        for (int i = 0; i < len1 + len2; i++) {
+            if (i >= len1) {
+                x1 = x1 * 10 + (num2.charAt(i - len1) - '0');
             } else {
-                idx1 = i + step;
-                idx2 = i;
+                x1 = x1 * 10 + (num1.charAt(i) - '0');
             }
-            c1 = num1.charAt(idx1);
-            c2 = num2.charAt(idx2);
-            if (c1 > c2) {
+            if (i >= len2) {
+                x2 = x2 * 10 + (num1.charAt(i - len2) - '0');
+            } else {
+                x2 = x2 * 10 + (num2.charAt(i) - '0');
+            }
+            if (x1 > x2) {
                 return 1;
             }
-            if (c1 < c2) {
+            if (x2 > x1) {
                 return -1;
-            }
-            i++;
-        }
-        if (maxLen > step + right) {
-            return dfsCompare(num1, num2, maxLen, minLen, step + minLen);
-        } else if (maxLen == step + right) {
-            int j = 0;
-            int idx3, idx4;
-            char c3, c4;
-            while (j < minLen - right) {
-                if (_1less_2) {
-                    idx3 = j;
-                    idx4 = right + j;
-                } else {
-                    idx3 = right + j;
-                    idx4 = j;
-                }
-                c3 = num1.charAt(idx3);
-                c4 = num2.charAt(idx4);
-                if (c3 > c4) {
-                    return 1;
-                }
-                if (c3 < c4) {
-                    return 1;
-                }
-                j++;
             }
         }
         return 0;
